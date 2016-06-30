6f620fef567705b5bd1afeb8b28f52f6f42c55ca
Zinonas Antoniou <z.antoniou@gmail.com>
2016-06-30 15:05:43 +0300

    Debug false

--- src/project_mytif/urls.py
+++ src/project_mytif/urls.py
@@ -56,7 +56,7 @@ urlpatterns = patterns('',
 )
 
 #if DEBUG is true 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += staticfiles_urlpatterns()
+#if settings.DEBUG:
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
